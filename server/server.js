1d1d71b1db5100d04cfe029e83664472fb252a6f
tfavro <wetr9902@gmail.com>
2022-04-17 12:40:31 -0500

    server running graphql

--- server/server.js
+++ server/server.js
@@ -1,21 +1,69 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req, connection }) => {
+      if (connection) {
+       // check connection for metadata
+       return connection.context;
+      } else {
+       // check from req
+       const token = req.headers.authorization
+
+
+      if(token !== "null"){
+        try{
+
+        //validate user in client.
+        const currentUser = await jwt.verify(token, process.env.SECRET);
+
+        //add user to request
+        req.currentUser = currentUser;
+
+        return {
+            currentUser
+        }   
+      }catch(err){
+          return "";
+              }
+            }
+          }
+        }
+
+       
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
